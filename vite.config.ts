ae5ebeea876cc3191abcada096346df7c72f1a8a
Matthieu senosiain <senosiain.matthieu@yahoo.fr>
2025-07-03 21:42:32 +0200

    netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
