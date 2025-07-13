65727a9e3b201ff194f124553ee3e8b1dae3d2ba
Matthieu senosiain <senosiain.matthieu@yahoo.fr>
2025-07-13 09:04:52 +0200

    hero animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,42 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
+
+// Découpe chaque caractère
+const split = (text: string) => text.split("");
+
+const letter = {
+  hidden: { opacity: 0 },
+  visible: (i: number) => ({
+    opacity: 1,
+    transition: {
+      delay: i * 0.06,
+    },
+  }),
+};
 
 const Hero = () => {
-  return <div className="h-80 w-full">banner</div>;
+  const fullName = split("Senosiain\u00A0Matthieu");
+
+  return (
+    <section className="flex h-80 w-full items-center justify-center">
+      <div className="space-y-1 text-5xl leading-none font-semibold">
+        <motion.span initial="hidden" animate="visible" className="block">
+          {fullName.map((char, i) => (
+            <motion.span
+              key={`char-${i}`}
+              custom={i}
+              variants={letter}
+              className="inline-block"
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.span>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
