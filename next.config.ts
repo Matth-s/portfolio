f6a6fb40384210bde260e180a304ab0c87bcf2a2
Matthieu senosiain <senosiain.matthieu@yahoo.fr>
2025-07-13 18:59:12 +0200

    security header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Toutes les routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "fullscreen=(self)",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
